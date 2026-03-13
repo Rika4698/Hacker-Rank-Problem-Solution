0e83266f82433101a1b0cc0d7e9900bbab1d0cf8
Rika4698 <rekasharmin46@gmail.com>
2026-03-14 01:23:04 +0600

    employee-validation

---
+++ employee-validation/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import "h8k-components";
+import EmployeeValidationForm from "./components/EmployeeValidationForm";
+
+const title = "Employee Validation";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <EmployeeValidationForm />
+    </div>
+  );
+};
+
+export default App;
