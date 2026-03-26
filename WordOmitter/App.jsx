cb835bcf2ba4f7ebf70f4ae111bc90287caed2d4
Rika4698 <rekasharmin46@gmail.com>
2026-03-26 20:53:48 +0600

    WordOmitter

---
+++ WordOmitter/App.jsx
@@ -0,0 +1,18 @@
+import { WordOmitter } from "./components/wordOmitter";
+
+import "./App.css";
+
+import "h8k-components";
+
+function App() {
+  return (
+    <>
+      <h8k-navbar header="Word Omitter"></h8k-navbar>
+      <div className="App">
+        <WordOmitter />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ WordOmitter/wordOmitter.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const OMITTED_WORDS = ["a", "the", "and", "or", "but"];
+
+function WordOmitter() {
+    const [inputText, setInputText] = useState("");
+    const [omitWords, setOmitWords] = useState(true);
+
+    const handleInputChange = (e) => {
+        setInputText(e.target.value);
+    };
+
+    const toggleOmitWords = () => {
+        setOmitWords(!omitWords);
+    };
+
+    const clearFields = () => {
+        // TODO: Add your changes here
+        setInputText("");
+        setOmitWords(true);
+    };
+
+    const getProcessedText = () => {
+        // TODO: Add your changes here
+        if (!inputText)
+            return "";
+        const words = inputText.split(" ");
+
+        if (omitWords) {
+            const filterWords = words.filter((word) => !OMITTED_WORDS.includes(word.toLocaleLowerCase()));
+            return filterWords.join(" ")
+        }
+        return inputText;
+    };
+
+    return (
+        <div className="omitter-wrapper">
+            <textarea
+                placeholder="Type here..."
+                value={inputText}
+                onChange={handleInputChange}
+                data-testid="input-area"
+            />
+            <div>
+                <button onClick={toggleOmitWords} data-testid="action-btn">
+                    {omitWords ? "Show All Words" : "Omit Words"}
+                </button>
+                <button onClick={clearFields} data-testid="clear-btn">
+                    Clear
+                </button>
+            </div>
+            <div>
+                <h2>Output:</h2>
+                <p data-testid="output-text">{getProcessedText()}</p>
+            </div>
+        </div>
+    );
+}
+
+export { WordOmitter };
