2e5782c10787ee4b525efc4cd58a8018880d3175
Rika4698 <rekasharmin46@gmail.com>
2026-08-31 23:48:29 +0600

    binary-search-tree-1

---
+++ SQL/binary-search-tree-1.sql
@@ -0,0 +1,15 @@
+/*
+Enter your query here.
+*/
+SELECT N , 
+   CASE 
+   WHEN P IS NULL THEN 'Root'
+   WHEN N IN (
+    SELECT DISTINCT P
+    FROM BST
+    WHERE P IS NOT NULL
+   ) THEN 'Inner'
+   ELSE 'Leaf'
+  END AS node_type
+FROM BST
+ORDER BY N;   
