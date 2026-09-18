b9c2c9e81f1aaddbd3fb4590e8563380cc33852f
Rika4698 <rekasharmin46@gmail.com>
2026-09-18 12:00:00 +0600

    weather-observation-station-12

--- SQL/weather-observation-station-12.sql
+++ SQL/weather-observation-station-12.sql
@@ -6,3 +6,30 @@ SELECT DISTINCT CITY
 FROM STATION 
 WHERE LEFT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U')
 AND RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U');
+
+-- RIGHT(CITY, 1)
+
+-- Here:
+
+-- CITY → the column
+-- 1 → take only one character
+-- RIGHT() → start from the right
+
+-- For example:
+
+-- London → n
+-- Dhaka  → a
+-- Tokyo  → o
+-- Paris  → s
+
+-- RIGHT(CITY, 1) IN ('A', 'E', 'I', 'O', 'U')
+
+-- means:
+
+-- The last character is a vowel.
+
+-- RIGHT(CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U')
+
+-- means:
+
+-- The last character is NOT a vowel.
